e6641451e933e9558cf907f78b066687557242d3
Hiacynthe Temgoua <temguahiacynthe@gmail.com>
2026-04-17 11:49:46 +0100

    fix: add prisma migrations

---
+++ prisma/migrations/20260417104847_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "gender_probability" DOUBLE PRECISION NOT NULL,
+    "sample_size" INTEGER NOT NULL,
+    "age" INTEGER NOT NULL,
+    "age_group" TEXT NOT NULL,
+    "country_id" TEXT NOT NULL,
+    "country_probability" DOUBLE PRECISION NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_name_key" ON "Profile"("name");
